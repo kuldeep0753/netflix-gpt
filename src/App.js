20cacb2426e03c9fcf397bff39a36e385787ff20
Kuldeep Singh <kushwaha0753kuldeep@gmail.com>
2025-08-01 15:52:04 +0530

    setup React project with Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App bg-gray-900 min-h-screen text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Hi !! It is my Netflix Clone</h1>
+    </div>
+  );
+}
+
+export default App;
